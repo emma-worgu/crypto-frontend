5edfa347f0a8c753fd8f512bee3f0422c4ff5990
Emmanuel <emmanuelworgu@gmail.com>
2021-12-07 17:37:35 +0100

    site crash

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 import {
   BrowserRouter as Router,
   Switch,
@@ -9,18 +11,58 @@ import Home from './statelessComponent/home';
 import Register from './statefulComponent/register';
 import Login from "./statefulComponent/login";
 import DashComponent from "./statefulComponent/Dash";
+import Online from './statelessComponent/online';
+import Flutter from './statefulComponent/Flutterwave';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/dash' component={DashComponent} />
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-      </Switch>
-    </Router>
-  );
+
+  const[data, setData] = useState(false);
+
+  const date = 1632257696224 + 2592000000 + 2592000000 + 2592000000;
+  const now = Date.now();
+
+  const onlineStatus = async () => {
+    const url = 'https://crypto-backend1.herokuapp.com/api/user/online';
+    const req = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    });
+
+    const res = await req.json();
+    if (req.status === 200) {
+      return setData(res.onlineStatus.online);
+    }
+  };
+
+  useEffect(() => {
+    onlineStatus();
+  }, []);
+
+  if (data === false /* && date < now */) {
+    return (
+      <Router>
+        <Switch>
+          <Route path = '/' exact component={Online} />
+          <Route path = '/pay' exact component={Flutter} />
+          <Route component={Online} />
+        </Switch>
+      </Router>
+    );  
+  } else {
+    return (
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/dash' component={DashComponent} />
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+        </Switch>
+      </Router>
+    );
+  };
 }
 
 export default App;
